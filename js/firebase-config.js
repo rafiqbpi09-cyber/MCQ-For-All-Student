6e4fbacb3637dd979eefabe93ba4cd38d0b1a458
rafiqbpi09-cyber <rafiqbpi09@gmail.com>
2026-09-05 07:20:32 -0700

    Add Firebase configuration and initialization

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDM13kU5-lT7DSlsCp2tVCZkMVAymlzHNg",
+  authDomain: "mcq-6to12.firebaseapp.com",
+  projectId: "mcq-6to12",
+  storageBucket: "mcq-6to12.firebasestorage.app",
+  messagingSenderId: "784808401512",
+  appId: "1:784808401512:web:7ab4aaa43f20aeab9739dd"
+};
 
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
